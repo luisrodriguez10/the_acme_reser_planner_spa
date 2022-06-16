3be943d676c6df9709e67e278f2a27197d70e6a4
Luis Rodriguez <luisu_10@hotmail.com>
2022-06-16 16:25:07 -0500

    added render users and restaurants

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log('ready with webpack')
+const axios = require("axios");
+
+const restaurantsList = document.querySelector("#restaurants-list");
+const usersList = document.querySelector("#users-list");
+const reservationsList = document.querySelector("#reservations-list");
+
+const state = {};
+
+const fetchUsers = async () => {
+  const response = await axios.get("/api/users");
+  state.users = response.data;
+};
+
+const renderUsers = () => {
+  const html = state.users
+    .map((user) => {
+      return `
+            <li>
+                <a href='#${user.id}'>
+                    ${user.name}
+                </a>
+            </li>
+        `;
+    })
+    .join("");
+    usersList.innerHTML = html
+};
+
+const fetchRestaurants = async () => {
+  const response = await axios.get("/api/restaurants");
+  state.restaurants = response.data;
+};
+
+const renderRestaurants = () => {
+  const html = state.restaurants
+    .map((restaurant) => {
+      return `
+            <li>
+                ${restaurant.name}
+            </li>
+        `;
+    })
+    .join("");
+    restaurantsList.innerHTML = html
+};
+
+const start = async () => {
+  await fetchUsers();
+  await fetchRestaurants();
+  renderUsers();
+  renderRestaurants();
+};
+
+start();
